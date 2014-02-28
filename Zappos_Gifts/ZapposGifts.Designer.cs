12d4f8f522d1f9573d52fc0fec824c715a50323f
unknown <abriskiev@R0708389.campus.ltu.edu>
2014-02-28 00:56:17 -0500

    First Commit

---
+++ Zappos_Gifts/ZapposGifts.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Zappos_Gifts
+{
+    partial class ZapposGifts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumberProducts = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lbProduct = new System.Windows.Forms.Label();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.lbTotal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(58, 134);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(110, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search for your Gift!";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "# of products:";
+            // 
+            // txtNumberProducts
+            // 
+            this.txtNumberProducts.Location = new System.Drawing.Point(105, 38);
+            this.txtNumberProducts.Name = "txtNumberProducts";
+            this.txtNumberProducts.Size = new System.Drawing.Size(100, 20);
+            this.txtNumberProducts.TabIndex = 1;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(105, 64);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPrice.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "price:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(222, 38);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(288, 317);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // lbProduct
+            // 
+            this.lbProduct.AutoSize = true;
+            this.lbProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbProduct.Location = new System.Drawing.Point(26, 101);
+            this.lbProduct.Name = "lbProduct";
+            this.lbProduct.Size = new System.Drawing.Size(127, 13);
+            this.lbProduct.TabIndex = 6;
+            this.lbProduct.Text = "Inform # of Products!";
+            this.lbProduct.Visible = false;
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPrice.Location = new System.Drawing.Point(26, 101);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(75, 13);
+            this.lbPrice.TabIndex = 4;
+            this.lbPrice.Text = "InformPrice!";
+            this.lbPrice.Visible = false;
+            // 
+            // lbTotal
+            // 
+            this.lbTotal.AutoSize = true;
+            this.lbTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTotal.Location = new System.Drawing.Point(228, 362);
+            this.lbTotal.Name = "lbTotal";
+            this.lbTotal.Size = new System.Drawing.Size(73, 13);
+            this.lbTotal.TabIndex = 7;
+            this.lbTotal.Text = "Total Price:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Your Gift";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTotal.Enabled = false;
+            this.txtTotal.Location = new System.Drawing.Point(307, 362);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(100, 13);
+            this.txtTotal.TabIndex = 9;
+            // 
+            // ZapposGifts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 386);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbTotal);
+            this.Controls.Add(this.lbPrice);
+            this.Controls.Add(this.lbProduct);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumberProducts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "ZapposGifts";
+            this.Text = "ZapposGifts";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumberProducts;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lbProduct;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label lbTotal;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
---
+++ Zappos_Gifts/ZapposGifts.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Zappos_Gifts
+{
+    public partial class ZapposGifts : Form
+    {
+        const string key = "52ddafbe3ee659bad97fcce7c53592916a6bfd73"; // Zappos API key to make the calls
+        const Int16 maxArray = Int16.MaxValue;                         // Maximum array indexes
+
+
+        public ZapposGifts()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            #region Variable Declaration
+            ProductList[] productList = new ProductList[maxArray];
+            ProductCombinationList[] productCombinationList = new ProductCombinationList[maxArray];
+            int numberProducts = 0;              // Number of Products
+            decimal price = 0;                   // Price for all the products
+            //int totalResultCount = 0;          // Max Result Count
+            #endregion
+
+            #region Obtain Values
+            try
+            {
+                numberProducts = Convert.ToInt32(txtNumberProducts.Text);
+            }
+            catch (Exception ex)
+            {
+                lbProduct.Visible = true;
+                lbPrice.Visible = false;
+                return;
+            }
+            Console.WriteLine("\nInform desired price:");
+            try
+            {
+                price = Convert.ToDecimal(txtPrice.Text);
+            }
+            catch (Exception ex)
+            {
+                lbPrice.Visible = true;
+                lbProduct.Visible = false;
+                return;
+            }
+
+            if (price == 0)
+            {
+                lbPrice.Visible = true;
+                lbProduct.Visible = false;
+                return;
+            }
+            else
+                lbPrice.Visible = false;
+
+            if (numberProducts == 0)
+            {
+                lbProduct.Visible = true;
+                lbProduct.Visible = false;
+                return;
+            }
+            else
+                lbProduct.Visible = false;
+
+            #endregion
+
+            #region Obtain List of products grouping by price
+            string endPoint = @"http://api.zappos.com/Search?" + "&key=" + key +
+                                                                 "&sort={\"price\":\"asc\"}&facets=[\"price\"]&facetSort=name";
+
+            var client = new RestClient(endPoint);
+            var json = client.MakeRequest();
+
+            JObject o = JObject.Parse(json);
+            int totalProducts = 0; // counter for the Products
+            int totalLines = 0;
+            string products;
+            string[] productsParsed = new string[Int16.MaxValue];
+            foreach (KeyValuePair<String, JToken> d in o)
+            {
+                if (d.Key == "facets")
+                {
+                    products = Convert.ToString(d.Value);
+                    products = products.Replace("\n", "").Replace("\r", "").Replace("[", "").Replace(" ", "").Replace("],", "").Replace("]", "")
+                                       .Replace("{", "").Replace("}", "").Replace("\"values\"", "").Replace("\"name\"", "").Replace("\"count\":", "")
+                                       .Replace("\"facetFieldDisplayName\":\"\"", "").Replace("}]", "").Replace("\"", "").Replace("facetField:price", "");
+                    productsParsed = products.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                    totalLines = productsParsed.Length;
+                    break;
+                }
+            }
+
+            int countProduct = 0;
+            string[] values = new string[2];
+
+            for (totalProducts = 0; totalProducts < totalLines; totalProducts++)
+            {
+                values = productsParsed[totalProducts].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries); ;
+                if (values.Length != 0)
+                {
+                    ProductList productListObj = new ProductList();
+                    productListObj.price = Convert.ToDecimal(values[0] == "" ? "0" : values[0]);
+                    productListObj.count = Convert.ToInt16(values[1] == "" ? "0" : values[1]);
+                    productList[countProduct] = productListObj;
+                    countProduct++;
+                }
+            }
+
+            #endregion
+
+            #region Obtain best combination of products
+            decimal pricePerProduct = price / numberProducts;
+            productsParsed = new string[Int16.MaxValue];
+            int countProductCombination = 0;
+            decimal countPrice = 0;
+
+            // List of prices and count
+            for (int i = 0; i < countProduct; i++)
+            {
+                endPoint = @"http://api.zappos.com/Search?" + "&key=" + key +
+                                                                "&excludes=[\"styleId\",\"productUrl\",\"colorId\",\"thumbnailImageUrl\",\"productId\"]" +
+                                                                "&limit=100&filters={\"price\":[\"" + Convert.ToString(productList[i].price) + "\"]}";
+                var client2 = new RestClient(endPoint);
+                var json2 = client2.MakeRequest();
+
+                JObject o2 = JObject.Parse(json2);
+
+                foreach (KeyValuePair<String, JToken> d2 in o2)
+                {
+                    if (d2.Key == "results")
+                    {
+                        products = Convert.ToString(d2.Value);
+                        products = products.Replace("\n", "").Replace("\r", "").Replace("[", "").Replace(" ", "").Replace("],", "").Replace("]", "")
+                                            .Replace("{", "").Replace("}", "").Replace("\"price\"", "").Replace("\"originalPrice\":", "")
+                                            .Replace("\"productName\":", "").Replace("\"brandName\":", "").Replace("\"percentOff\":", "")
+                                            .Replace("}]", "").Replace("\"", "").Replace("},", "");
+                        productsParsed = products.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                        productsParsed = products.Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries); ;
+                        totalLines = productsParsed.Length;
+                        break;
+                    }
+                }
+
+                values = new string[5];
+
+                // List of products for current price
+                for (totalProducts = 0; totalProducts < totalLines; totalProducts++)
+                {
+                    values = productsParsed[totalProducts].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries); ;
+                    if (values.Length != 0)
+                    {
+                        ProductCombinationList productCombinationListObj = new ProductCombinationList();
+                        productCombinationListObj.price = values[0];
+                        productCombinationListObj.originalPrice = values[1];
+                        productCombinationListObj.productName = values[2];
+                        productCombinationListObj.brandName = values[3];
+                        productCombinationListObj.percentOff = values[4];
+                        productCombinationList[countProductCombination] = productCombinationListObj;
+                        countProductCombination++;
+                        countPrice += productList[i].price;
+                        if (countProductCombination >= numberProducts && countPrice >= price) // reached max # of products and total price
+                        {
+                            i = countProduct;
+                            break;
+                        }
+                        else if (countProductCombination >= numberProducts && countPrice < price) // reached max # of products but not the total price
+                        {
+                            countProductCombination--;
+                            countPrice -= productList[i].price;
+                            break;
+                        }                        
+                    }
+                }
+            }
+
+            #endregion
+
+            #region Print result
+
+            txtOutput.Text = "";
+            if (countProductCombination > 0)
+            {
+                decimal totalPrice = 0;
+
+                for (totalProducts = 0; totalProducts < countProductCombination; totalProducts++)
+                {
+                    txtOutput.Text += "Product Name: " + productCombinationList[totalProducts].productName + "\r\n";
+                    txtOutput.Text += "Brand Name: " + productCombinationList[totalProducts].brandName + "\r\n";
+                    txtOutput.Text += "Original Price: " + productCombinationList[totalProducts].originalPrice + "\r\n";
+                    txtOutput.Text += "Price: " + productCombinationList[totalProducts].price + "\r\n";
+                    txtOutput.Text += "Percent Off: " + productCombinationList[totalProducts].percentOff + "\r\n\r\n";
+                    totalPrice += Convert.ToDecimal(productCombinationList[totalProducts].price.Substring(1, productCombinationList[totalProducts].price.Length - 1));
+                }
+                txtOutput.Text += "Total Price: " + Convert.ToString(totalPrice);
+                txtTotal.Text = Convert.ToString(totalPrice);
+            }
+            else
+            {
+                txtOutput.Text = "Couldn't find a good combination! Try a different # of products or price!";
+                txtTotal.Text = "";
+            }
+
+            #endregion
+        }
+    }
+
+    public class ProductList
+    {
+        public decimal price { get; set; }
+        public int count { get; set; }
+    }
+
+    public class ProductCombinationList
+    {
+        public string price { get; set; }
+        public string originalPrice { get; set; }
+        public string productName { get; set; }
+        public string brandName { get; set; }
+        public string percentOff { get; set; }
+    }
+}
